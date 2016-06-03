12f636aa071bb224769274b62f141d210ea7d561
Zack Goyetche <zack.goyetche@gmail.com>
2016-06-03 17:59:34 -0400

    Config for dsPIC33EP512GM306

---
+++ config.h
@@ -0,0 +1,52 @@
+// DSPIC33EP512GM306 Configuration Bit Settings
+
+#include <xc.h>
+
+// FICD
+#pragma config ICS = PGD1              // ICD Communication Channel Select bits (Communicate on PGEC2 and PGED2)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+
+// FPOR
+#pragma config BOREN = ON               // Brown Out Reset enabled
+#pragma config ALTI2C1 = OFF            // Alternate I2C1 pins (I2C1 mapped to SDA1/SCL1 pins)
+#pragma config ALTI2C2 = OFF            // Alternate I2C2 pins (I2C2 mapped to SDA2/SCL2 pins)
+#pragma config WDTWIN = WIN25           // Watchdog Window Select bits (WDT Window is 25% of WDT period)
+
+// FWDT
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler bits (1:32,768)
+#pragma config WDTPRE = PR128           // Watchdog Timer Prescaler bit (1:128)
+#pragma config PLLKEN = ON              // PLL Lock Enable bit (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF              // Watchdog Timer Enable bit (Watchdog timer always enabled)
+
+// FOSC
+#pragma config POSCMD = HS              // Primary Oscillator Mode Select bits (HS Crystal Oscillator Mode)
+#pragma config OSCIOFNC = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
+#pragma config IOL1WAY = ON             // Peripheral pin select configuration (Allow only one reconfiguration)
+#pragma config FCKSM = CSECMD           // Clock Switching Mode bits (Clock switching is enabled,Fail-safe Clock Monitor is disabled)
+
+// FOSCSEL
+#pragma config FNOSC = FRCDIVN          // Oscillator Source Selection (Internal Fast RC (FRC) Oscillator with postscaler)
+#pragma config PWMLOCK = ON             // PWM Lock Enabled
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FGS
+#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+#pragma config GCP = OFF                // General Segment Code-Protect bit (General Segment Code protect is Disabled)
+
+#ifndef __DELAY_H
+
+	#define FOSC        140000000LL
+    #define FCY      	(FOSC/2)		// MCU is running at FCY MIPS
+
+    #define delay_us(x)	__delay32(((x*FCY)/1000000L))	// delays x us
+    #define delay_ms(x)	__delay32(((x*FCY)/1000L))		// delays x ms
+
+	#define __DELAY_H	1
+
+#endif
+
+ /*
+ *      Library Files
+ *          /opt/microchip/xc16/v1.25/lib/libdsp-elf-a
+ */
